93c99a72fe4d9898af5b1875f4b3ba855df311e2
andrei <andrei@192.168.1.24>
2019-02-26 19:29:16 +0200

    BA9

--- AndreiSavnet/src/pkgandrei/TestBA9.java
+++ AndreiSavnet/src/pkgandrei/TestBA9.java
@@ -3,18 +3,14 @@ package pkgandrei;
 public class TestBA9 {
 	public static int getDiagonalSum(int[][] matrix) {
 		int sum = 0;
-		for(int i=0;i<matrix.length;i++) {
-			for(int j=0;j<matrix.length;j++) {
-				if(i==j) {
-					sum = sum + matrix[i][j];
-				}
-			}
+		for (int i = 0; i < matrix.length; i++) {
+			sum = sum + matrix[i][i];
 		}
 		return sum;
 	}
-	
+
 	public static void main(String[] args) {
-		System.out.println(getDiagonalSum(new int[][]{ {4,6,3}, {0,2,1}, {6,8,5} }));
+		System.out.println(getDiagonalSum(new int[][] { { 4, 6, 3 }, { 0, 2, 1 }, { 6, 8, 5 } }));
 	}
-	 
+
 }
