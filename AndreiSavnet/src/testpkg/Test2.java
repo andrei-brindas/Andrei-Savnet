e94ee8d4ef2ec73d65e4122c1333cdbc80f0fe43
andrei <andrei@192.168.88.131>
2019-03-19 18:46:47 +0200

    ex 2 test

--- AndreiSavnet/src/testpkg/Test2.java
+++ AndreiSavnet/src/testpkg/Test2.java
@@ -3,7 +3,6 @@ package testpkg;
 public class Test2 {
 	public static boolean isSorted(int[] arr) {
 		for (int i = 0; i < arr.length - 1; i++) {
-
 			if (arr[i] > arr[i + 1]) {
 				return false;
 			}
